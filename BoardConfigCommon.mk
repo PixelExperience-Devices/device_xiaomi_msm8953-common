2d35dab7beb1bd64940af06fab8a187b2719b195
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-13 16:41:34 -0300

    msm8953-common: Enable TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -213,5 +213,8 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# Telephony
+TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
