8445541fddaf1330503076717fffc3c5ded61fe4
Wight554 <wight554@gmail.com>
2019-09-11 18:32:26 +0300

    msm8953-common: nuke lineage sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,6 +191,7 @@ endif
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+WITHOUT_LINEAGE_SEPOLICY := true
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
