22de82294e29cb16cede195ff5e5f4b748494001
PrateekPunetha <prateekpunetha@gmail.com>
2019-09-12 20:28:46 +0300

    Revert "msm8953-common: drop lineage specific services"
    This reverts commit 61b3296861d289705f897e431ace1bdd8bd19bc9.

--- msm8953.mk
+++ msm8953.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8953
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml::$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -259,6 +263,9 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.1-service-qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
