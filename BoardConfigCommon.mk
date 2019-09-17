21b0a502a5efdf7564226c9380ca73b8f634bb7c
PrateekPunetha <prateekpunetha@gmail.com>
2019-09-17 15:58:06 +0300

    Revert "msm8953-common: nuke lineage sepolicy"
    This reverts commit 8445541fddaf1330503076717fffc3c5ded61fe4.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,6 @@ endif
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
-WITHOUT_LINEAGE_SEPOLICY := true
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
