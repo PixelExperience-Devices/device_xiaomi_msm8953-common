2475203bbdd7a104ccac491e1c351e8c8fdd2243
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-01-13 00:18:25 +0530

    msm8953-common: Nuke enforce RRO for all targets
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: Iff5c594cda7e42aa5e1fe67102c082571e4085a3

--- msm8953.mk
+++ msm8953.mk
@@ -20,8 +20,6 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
