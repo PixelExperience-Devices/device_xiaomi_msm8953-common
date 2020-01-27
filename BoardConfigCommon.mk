51bea9637d4584b65a56678e57c817ef15033e74
Pranav Vashi <neobuddy89@gmail.com>
2020-01-27 07:02:04 +0530

    msm8953-common: Fix path for libinit_msm8953
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,7 @@ DEVICE_MATRIX_FILE   := $(VENDOR_PATH)/compatibility_matrix.xml
 HWUI_COMPILE_FOR_PERF := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm8953
+TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_msm8953
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
 # Lights
