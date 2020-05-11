bb2a88b71e964dcfc7b8cf5b1eecfb50a5771f83
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2020-05-11 05:40:53 +0100

    msm8953: Build with clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,10 +47,8 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-ifneq ($(TARGET_KERNEL_SOURCE),)
-    TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
-endif
 TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
