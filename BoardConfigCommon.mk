e4437a473f86aea01c645b088b6b3a80047fcc04
4PERTURE <slicey2001@gmail.com>
2020-05-14 10:27:41 +0100

    msm8953: fix zygote denial
    This fixes
    [avc: denied { mounton } for path="/" dev="mmcblk0p24" ino=2 scontext=u:r:zygote:s0 tcontext=u:object_r:system_file:s0 tclass=dir permissive=0]
    Which causes the device to stop booting after making a backup and restoring it.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -204,6 +204,7 @@ endif
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
