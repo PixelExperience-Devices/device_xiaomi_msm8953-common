8312ab8325f0431df5e4b857cb996327e719a2f9
abhinavgupta371 <abhinavgupta371@gmail.com>
2020-09-18 14:39:12 +0200

    Revert "msm8953-common: build kernel using proton"
    This reverts commit 7771b9341f7a06218812bf0546df326c0afa3aa1.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,9 +46,6 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_VERSION := 4.9
 
 # ANT
