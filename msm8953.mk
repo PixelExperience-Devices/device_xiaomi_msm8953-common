4544b1267f345a4cb35a3e43af270844dae7890c
abhinavgupta371 <abhinavgupta371@gmail.com>
2021-03-10 17:41:58 +0000

    msm8953-common: Drop overlay-lineage
     Add AOSP overlays as well

--- msm8953.mk
+++ msm8953.mk
@@ -9,7 +9,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
