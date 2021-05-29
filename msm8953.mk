5b75adf45d93532037eb1876e50237fb8d311d74
abhinavgupta371 <abhinavgupta371@gmail.com>
2021-05-29 11:24:16 +0000

    msm8953-common: Update WFD from nokia
    Change-Id: Ic6bd4f987ea2b743c4ce30e4cc45558cde8f09c6

--- msm8953.mk
+++ msm8953.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
-    libqdMetaData.system \
+    libqdMetaData \
+    libqdMetaData.vendor \
     libgenlock \
     libtinyxml
 
@@ -406,7 +407,8 @@ PRODUCT_COPY_FILES += \
 
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
-    libnl
+    libnl \
+    libstagefright_enc_common
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
